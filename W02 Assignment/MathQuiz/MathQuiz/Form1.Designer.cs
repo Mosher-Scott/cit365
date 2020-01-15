f770068852dbf9d9f0994c06cd9301ebf1a30d0f
Mosher-Scott <46462152+Mosher-Scott@users.noreply.github.com>
2020-01-14 21:30:18 -0700

    Finished step 1 & 2

--- W02 Assignment/MathQuiz/MathQuiz/Form1.Designer.cs
+++ W02 Assignment/MathQuiz/MathQuiz/Form1.Designer.cs
@@ -31,6 +31,31 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            this.difference = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.minusRightLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.minusLeftLabel = new System.Windows.Forms.Label();
+            this.product = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timesRightLabel = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timesLeftLabel = new System.Windows.Forms.Label();
+            this.quotient = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dividedRightLabel = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dividedLeftLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -46,14 +71,148 @@
             this.label2.Name = "label2";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // plusLeftLabel
+            // 
+            resources.ApplyResources(this.plusLeftLabel, "plusLeftLabel");
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // plusRightLabel
+            // 
+            resources.ApplyResources(this.plusRightLabel, "plusRightLabel");
+            this.plusRightLabel.Name = "plusRightLabel";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // sum
+            // 
+            resources.ApplyResources(this.sum, "sum");
+            this.sum.Name = "sum";
+            // 
+            // difference
+            // 
+            resources.ApplyResources(this.difference, "difference");
+            this.difference.Name = "difference";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // minusRightLabel
+            // 
+            resources.ApplyResources(this.minusRightLabel, "minusRightLabel");
+            this.minusRightLabel.Name = "minusRightLabel";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // minusLeftLabel
+            // 
+            resources.ApplyResources(this.minusLeftLabel, "minusLeftLabel");
+            this.minusLeftLabel.Name = "minusLeftLabel";
+            // 
+            // product
+            // 
+            resources.ApplyResources(this.product, "product");
+            this.product.Name = "product";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // timesRightLabel
+            // 
+            resources.ApplyResources(this.timesRightLabel, "timesRightLabel");
+            this.timesRightLabel.Name = "timesRightLabel";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // timesLeftLabel
+            // 
+            resources.ApplyResources(this.timesLeftLabel, "timesLeftLabel");
+            this.timesLeftLabel.Name = "timesLeftLabel";
+            // 
+            // quotient
+            // 
+            resources.ApplyResources(this.quotient, "quotient");
+            this.quotient.Name = "quotient";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // dividedRightLabel
+            // 
+            resources.ApplyResources(this.dividedRightLabel, "dividedRightLabel");
+            this.dividedRightLabel.Name = "dividedRightLabel";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // dividedLeftLabel
+            // 
+            resources.ApplyResources(this.dividedLeftLabel, "dividedLeftLabel");
+            this.dividedLeftLabel.Name = "dividedLeftLabel";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.quotient);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.dividedRightLabel);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.dividedLeftLabel);
+            this.Controls.Add(this.product);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timesRightLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.timesLeftLabel);
+            this.Controls.Add(this.difference);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.minusRightLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.minusLeftLabel);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.plusLeftLabel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.ScottMosherMathQuiz_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,6 +222,27 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown sum;
+        private System.Windows.Forms.NumericUpDown difference;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label minusRightLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label minusLeftLabel;
+        private System.Windows.Forms.NumericUpDown product;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label timesRightLabel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label timesLeftLabel;
+        private System.Windows.Forms.NumericUpDown quotient;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label dividedRightLabel;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label dividedLeftLabel;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- W02 Assignment/MathQuiz/MathQuiz/Form1.cs
+++ W02 Assignment/MathQuiz/MathQuiz/Form1.cs
@@ -12,7 +12,31 @@ namespace MathQuiz
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+     // Create a Random object called randomizer 
+     // to generate random numbers.
+    Random randomizer = new Random();
+
+        int addend1, addend2;
+
+    /// <summary>
+    /// Star the quiz by getting all the problems, and starting the timer
+    /// </summary>
+    public void StartTheQuiz()
+        {
+            // Fill in the adition problem by generationg 2 numbers to add.  Store them in the variables
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            // Convert the ints to a string
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            // This is the numeric control.  The will make sure the value is 0 when starting the problem
+            sum.Value = 0;
+        }
+
+    public Form1()
         {
             InitializeComponent();
         }
@@ -23,6 +47,17 @@ namespace MathQuiz
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void ScottMosherMathQuiz_Load(object sender, EventArgs e)
         {
 
         }
